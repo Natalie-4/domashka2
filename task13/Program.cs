a90290ccc1586622dca5cfab673ebeaa16d57f7a
Natalie <itlaym@yandex.ru>
2022-09-24 23:45:26 +0700

    solve task 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Введите число: ");
+string? chislo = Console.ReadLine();
+int dlina = chislo.Length;
+int number = Convert.ToInt32(chislo);
+
+if (number<100)
+{
+  Console.WriteLine("Третьей цифры нет");  
+}
+else 
+{
+for (int count = 0; count < dlina - 3; count++)
+{
+  number = number/10;
+}
+int digit3 = number%10;
+Console.WriteLine(digit3);
+}
