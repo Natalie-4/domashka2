5810dd465ad469b73b6b980f89af406682b55e8f
Natalie <itlaym@yandex.ru>
2022-09-24 19:03:30 +0700

    solve task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+Console.WriteLine("Введите трёхзначное число: ");
+int NumberDigit3 = Convert.ToInt32(Console.ReadLine());
+int NumberDigit2 = NumberDigit3/10;
+int TwoDigit = NumberDigit2%10;
+Console.WriteLine(TwoDigit);
+
